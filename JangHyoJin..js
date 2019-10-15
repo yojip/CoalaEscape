90d13d85ad0bf01b868d4cbb95f93f2223adb479
yojip <55662375+yojip@users.noreply.github.com>
2019-10-15 23:50:05 +0900

    Add files via upload

---
+++ JangHyoJin..js
@@ -0,0 +1,250 @@
+﻿Function.prototype.member = function(name, value){
+	this.prototype[name] = value
+}
+
+//////// Game Definition
+function Game(){}
+Game.start = function(room, welcome){
+	game.start(room.id)
+	printMessage(welcome)
+}
+Game.end = function(){
+	game.clear()
+}
+Game.move = function(room){
+	game.move(room.id)	
+}
+Game.handItem = function(){
+	return game.getHandItem()
+}
+
+
+//////// Room Definition
+
+function Room(name, background){
+	this.name = name
+	this.background = background
+	this.id = game.createRoom(name, background)
+}
+Room.member('setRoomLight', function(intensity){
+	this.id.setRoomLight(intensity)
+})
+
+//////// Object Definition
+
+function Object(room, name, image){
+	this.room = room
+	this.name = name
+	this.image = image
+
+	if (room !== undefined){
+		this.id = room.id.createObject(name, image)
+	}
+}
+Object.STATUS = { OPENED: 0, CLOSED: 1, LOCKED: 2 }
+
+Object.member('setSprite', function(image){
+	this.image = image
+	this.id.setSprite(image)
+})
+Object.member('resize', function(width){
+	this.id.setWidth(width)
+})
+Object.member('setDescription', function(description){
+	this.id.setItemDescription(description)
+})
+
+Object.member('getX', function(){
+	return this.id.getX()
+})
+Object.member('getY', function(){
+	return this.id.getY()
+})
+Object.member('locate', function(x, y){
+	this.room.id.locateObject(this.id, x, y)
+})
+Object.member('move', function(x, y){
+	this.id.moveX(x)
+	this.id.moveY(y)
+})
+
+Object.member('show', function(){
+	this.id.show()
+})
+Object.member('hide', function(){
+	this.id.hide()
+})
+Object.member('open', function(){
+	this.id.open()
+})
+Object.member('close', function(){
+	this.id.close()
+})
+Object.member('lock', function(){
+	this.id.lock()
+})
+Object.member('unlock', function(){
+	this.id.unlock()
+})
+Object.member('isOpened', function(){
+	return this.id.isOpened()
+})
+Object.member('isClosed', function(){
+	return this.id.isClosed()
+})
+Object.member('isLocked', function(){
+	return this.id.isLocked()
+})
+Object.member('pick', function(){
+	this.id.pick()
+})
+Object.member('isPicked', function(){
+	return this.id.isPicked()
+})
+
+//////// Door Definition
+
+function Door(room, name, closedImage, openedImage, connectedTo){
+	Object.call(this, room, name, closedImage)
+
+	// Door properties
+	this.closedImage = closedImage
+	this.openedImage = openedImage
+	this.connectedTo = connectedTo
+}
+// inherited from Object
+Door.prototype = new Object()
+
+Door.member('onClick', function(){
+	if (!this.id.isLocked() && this.id.isClosed()){
+		this.id.open()
+	}
+	else if (this.id.isOpened()){
+		if (this.connectedTo !== undefined){
+			Game.move(this.connectedTo)
+		}
+		else {
+			Game.end()
+		}
+	}
+})
+Door.member('onOpen', function(){
+	this.id.setSprite(this.openedImage)
+})
+Door.member('onClose', function(){
+	this.id.setSprite(this.closedImage)
+})
+
+
+//////// Keypad Definition
+
+function Keypad(room, name, image, password, callback){
+	Object.call(this, room, name, image)
+
+	// Keypad properties
+	this.password = password
+	this.callback = callback
+}
+// inherited from Object
+Keypad.prototype = new Object()
+
+Keypad.member('onClick', function(){
+	showKeypad('number', this.password, this.callback)
+})
+
+
+//////// DoorLock Definition
+function DoorLock(room, name, image, password, door, message){
+	Keypad.call(this, room, name, image, password, function(){
+		printMessage(message)
+		door.unlock()
+	})
+}
+// inherited from Object
+DoorLock.prototype = new Keypad()
+
+/////// Item Definition
+
+function Item(room, name, image){
+	Object.call(this, room, name, image)
+}
+// inherited from Object
+Item.prototype = new Object()
+
+Item.member('onClick', function(){
+	this.id.pick()
+})
+Item.member('isHanded', function(){
+	return Game.handItem() == this.id
+})
+
+
+
+
+
+
+
+
+room1 = new Room('room1', '배경-1.png')		// 변수명과 이름이 일치해야 한다.
+room2 = new Room('room2', '배경-7.png')		// 변수명과 이름이 일치해야 한다.
+room3 = new Room('room3', '배경-3.png')		// 변수명과 이름이 일치해야 한다.
+
+room1.door1 = new Door(room1, 'door1', '문-오른쪽-닫힘.png', '문-오른쪽-열림.png', room2)
+room1.door1.resize(120)
+room1.door1.locate(1000, 312)
+
+room1.key1 = new Item(room1, 'key1', '열쇠.png')
+room1.key1.resize(45)
+room1.key1.locate(745, 500)
+
+room1.door1.onClick = function(){
+	if (room1.key1.isHanded() && !this.id.isLocked() && this.id.isClosed()){
+		this.id.open()
+	}
+	else if (this.id.isOpened()){
+		if (this.connectedTo !== undefined){
+			Game.move(this.connectedTo)
+		}
+		else {
+			Game.end()
+		}
+	}
+}
+
+room2.door1 = new Door(room2, 'door1', '문-왼쪽-닫힘.png', '문-왼쪽-열림.png', room1)
+room2.door1.resize(120)
+room2.door1.locate(300, 270)
+
+room2.door2 = new Door(room2, 'door2', '문-오른쪽-닫힘.png', '문-오른쪽-열림.png', room3)
+room2.door2.resize(120)
+room2.door2.locate(1000, 305)
+room2.door2.hide()
+
+room2.keypad1 = new Keypad(room2, 'keypad1', '키패드-우.png', '4125', function(){
+	printMessage('어!! 문이 생겼네!')
+	room2.door2.show()
+})
+room2.keypad1.resize(20)
+room2.keypad1.locate(920, 250)
+
+// onClick 함수를 재정의할 수도 있다
+room2.keypad1.onClick = function(){
+	printMessage('★STAR★')
+	showKeypad('number', this.password, this.callback)
+}
+
+room3.door1 = new Door(room3, 'door1', '문-왼쪽-닫힘.png', '문-왼쪽-열림.png', room2)
+room3.door1.resize(120)
+room3.door1.locate(300, 297)
+
+room3.door2 = new Door(room3, 'door2', '문-오른쪽-닫힘.png', '문-오른쪽-열림.png')
+room3.door2.resize(120)
+room3.door2.locate(1000, 313)
+room3.door2.lock()
+
+room3.lock1 = new DoorLock(room3, 'lock1', '키패드-우.png', '1234', room3.door2, '철커덕')
+room3.lock1.resize(20)
+room3.lock1.locate(920, 250)
+
+
+Game.start(room1, '효진이의 방탈출에 온걸 환영해!')
